456c4e47c345b4613d34a76ba756f5a980c1790d
Nashville Software School <NashvilleSoftwareSchoolMac1@Nashvilles-MacBook-Pro.local>
2017-07-09 18:17:24 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+
+            transactions.Add(("hammer", 19.99, 10));
+            transactions.Add(("screwdriver", 11.99, 25));
+            transactions.Add(("power drill", 109.99, 7));
+            transactions.Add(("pliers", 8.99, 12));
+            transactions.Add(("pipe wrench", 12.99, 8));
+
+            double totalProducts = 0;
+            double totalRevenue = 0;
+
+            foreach ((string product, double amount, int quantity) t in transactions)
+            {
+                totalProducts += t.quantity;
+                totalRevenue += t.amount;
+            }
+                Console.WriteLine($"Items sold today {totalProducts}");
+                Console.WriteLine($"Total revenue: {totalProducts * totalRevenue}");
+        }
+    }
+}
